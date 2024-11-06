08bb27a3c040a47a8c04773239767231783a337f
Andre-deCampos-Silva <andre.silva.tvd@gmail.com>
2024-11-06 22:32:19 +0000

    JS - Funções onclick para formulário. HTML - file input e button onclick.

---
+++ pages/form/form_script.js
@@ -0,0 +1,7 @@
+function submeter() {
+    alert ("Informações enviadas com sucesso!\n\nSerá contactado em breve pela nossa equipa.");
+  }
+
+  function limpar() {
+    alert ("Todos os campos foram limpos.");
+  }
